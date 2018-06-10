bb33a22441c6022b0265b1cd6b5bedf4c18f1047
zhangran <zhangran@42.com>
2018-06-10 23:24:27 +0800

    loop queue

--- src/main/java/queue/LoopQueue.java
+++ src/main/java/queue/LoopQueue.java
@@ -30,12 +30,36 @@ public class LoopQueue<E> implements Queue<E> {
 
     @Override
     public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() * 2);
+        }
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
 
+    private void resize(int capacity) {
+        E[] newData = (E[]) new Object[capacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        front = 0;
+        tail = size;
     }
 
     @Override
     public E dequeue() {
-        return null;
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        E e = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+        return e;
     }
 
     @Override
@@ -50,6 +74,9 @@ public class LoopQueue<E> implements Queue<E> {
 
     @Override
     public E getFront() {
-        return null;
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return data[front];
     }
 }
