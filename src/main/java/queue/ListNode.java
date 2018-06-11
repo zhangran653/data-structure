6cf272a91972846f8c0a7f813df40869c5881fa5
zhangran <zhangran@42.com>
2018-06-11 20:05:41 +0800

    letcode

---
+++ src/main/java/queue/ListNode.java
@@ -0,0 +1,15 @@
+package queue;
+
+/**
+ * @author zhangran
+ * @since 2018-06-11
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
---
+++ src/main/java/queue/Solution.java
@@ -0,0 +1,54 @@
+package queue;
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode pre = head;
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                ListNode delNode = pre.next;
+                pre.next = pre.next.next;
+                delNode.next = null;
+            } else {
+                pre = pre.next;
+            }
+        }
+
+        return head;
+    }
+
+    public ListNode removeElements2(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode pre = dummyHead;
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                ListNode delNode = pre.next;
+                pre.next = pre.next.next;
+                delNode.next = null;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode n = new ListNode(1);
+
+        n.next = new ListNode(2);
+        s.removeElements(n, 1);
+
+    }
+}
