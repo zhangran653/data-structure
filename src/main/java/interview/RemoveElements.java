e77f49cf5fe4d435a8a39a219e64cdb50ce67a0a
zhangran <zhangran@42.com>
2018-11-23 18:29:31 +0800

    remove elements

---
+++ src/main/java/interview/RemoveElements.java
@@ -0,0 +1,47 @@
+package interview;
+
+/**
+ * https://leetcode.com/problems/remove-linked-list-elements/description/
+ *
+ * @author zhangran
+ * @since 2018-11-22
+ **/
+public class RemoveElements {
+    /**
+     * Remove all elements from a linked list of integers that have value val.
+     * <p>
+     * Example:
+     * <p>
+     * Input:  1->2->6->3->4->5->6, val = 6
+     * Output: 1->2->3->4->5
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            if (cur.val == val) {
+                if (pre == null) {
+                    head = head.next;
+                    cur = head;
+                } else {
+                    ListNode tempNext = cur.next;
+                    pre.next = cur.next;
+                    cur.next = null;
+                    cur = tempNext;
+                }
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
