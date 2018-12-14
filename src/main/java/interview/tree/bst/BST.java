1566813636e260560cb8a2db4ba63b6c51c5c5d7
zhangran <zhangran@42.com>
2018-12-14 18:01:54 +0800

    bst

--- src/main/java/interview/tree/bst/BST.java
+++ src/main/java/interview/tree/bst/BST.java
@@ -1,5 +1,8 @@
 package interview.tree.bst;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author zhangran
  * @since 2018-12-12
@@ -76,6 +79,68 @@ public class BST<E extends Comparable<E>> {
             }
         }
         return root;
+    }
+
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    private boolean contains(Node node, E e) {
+        if (e == null || node == null) {
+            return false;
+        }
+        if (node.e.equals(e)) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+
+    /**
+     * 1
+     * /  \
+     * 2    3
+     * / \   / \
+     * 4   5 5   6
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) {
+            return null;
+        }
+        sb.append(root.e);
+        List<Node> level = new ArrayList<>();
+        List<Node> next = new ArrayList<>();
+        level.add(root.left);
+        level.add(root.right);
+
+        while (!(level.stream().filter(c -> c == null).count() == level.size())) {
+            for (int i = 0; i < level.size(); i++) {
+                Node n = level.get(i);
+            }
+        }
+
+        return sb.toString();
+
     }
 
 
