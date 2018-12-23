1226ae775936b23af0cb9b722d447763037a34aa
zhangran <zhangran@42.com>
2018-12-23 16:55:53 +0800

    siftdown

--- src/main/java/interview/heap/MaxHeap.java
+++ src/main/java/interview/heap/MaxHeap.java
@@ -43,5 +43,52 @@ public class MaxHeap<E extends Comparable<E>> {
         return index * 2 + 2;
     }
 
+    public void add(E e) {
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+
+    private void siftUp(int k) {
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+
+    public E extractMax() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("heap empty");
+        }
+        E e = data.get(0);
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+        return e;
+    }
+
+    private void siftDown(int k) {
+        while (leftChild(k) < data.getSize()) {
+            int j = leftChild(k);
+            if (j + 1 < data.getSize() && data.get(j + 1).compareTo(data.get(j)) > 0) {
+                j = rightChild(k);
+            }
+            if (data.get(k).compareTo(data.get(j)) >= 0) {
+                break;
+            }
+            data.swap(j, k);
+            k = j;
+        }
+    }
+
+    public E replace(E e) {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("heap empty");
+        }
+        E ret = data.get(0);
+        data.set(0, e);
+        siftDown(0);
+        return ret;
+    }
+
 
 }
